d53618727d86cef4589aa4329a65cf83989a91c1
Garcia-17 <82557378+Garcia-17@users.noreply.github.com>
2025-01-01 17:16:57 -0700

    Initial commit

---
+++ Resources/Scripts/main.js
@@ -0,0 +1,9 @@
+export function sideOpen() {
+  document.getElementById("sideBar").style.display = "block";
+  element.classList.add("sideBarAnimation");
+}
+
+function sideClose() {
+  document.getElementById("sideBar").style.display = "none";
+  console.log("sideClose");
+}
